2e3d480ce3d6f4b08bec04046f0c881b6b1d8502
Louis Portay <lportay@student.42.fr>
2019-04-15 11:44:24 +0200

    changed memcpy implem

--- src/ft_memcpy.s
+++ src/ft_memcpy.s
@@ -2,19 +2,9 @@ SECTION .text
 	global _ft_memcpy
 
 _ft_memcpy:
-	push rbx
-	mov rax, rdi
-	mov rbx, rsi
-	xor rcx, rcx
-
-copy:
-	cmp byte [rbx], 0
-	je end
-	inc rbx
-	jmp copy
-
-end:
-	mov rcx, rdx
-	rep movsb
-	pop rbx
-	ret
+		push rdi
+		mov rcx, rdx
+		cld
+		repnz movsb
+		pop rax
+		ret
